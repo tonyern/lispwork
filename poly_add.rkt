9ba754359b3b7b08584099562d18b90203aca31c
Tony Nguyen <tony.d.nguyen-1@ou.edu>
2019-04-10 13:50:00 -0500

    Finished ladd.

--- poly_add.rkt
+++ poly_add.rkt
@@ -1,11 +1,14 @@
 #lang racket
 (define (poly_add x y)
+  ;(rem0atb (car x))
+  ;(rem0atb (car y))
   ; If x is not there then just output y.
   (if (null? x)
       y
   ; If y is not there then just output x.
   (if (null? y)
       x
+  ; Get a list of values from adding contents of x and y.
   (ladd (car x) (car y))))
   ; If x is not there then just output y.
   (if (null? x)
@@ -35,3 +38,10 @@
       x
   (if (= (car x) 0)
   (rem0atb (cdr x)) x)))
+
+; Adding stuff to a list.
+(define (revl l)
+  (if (null? l)
+      l
+  (append (revl (cdr l))
+  (list (car l)))))
--- poly_add_helper.rkt
+++ poly_add_helper.rkt
@@ -1,4 +1,29 @@
 #lang racket
-(define (add_helper x y)
-  (+ x y)
-  )
+(define (poly_add x y)
+  ; If x is not there then just output y.
+  (if (null? x)
+      y
+  ; If y is not there then just output x.
+  (if (null? y)
+      x
+  ; Get a list of values from adding contents of x and y.
+  (ladd (car x) (car y))))
+  ; If x is not there then just output y.
+  (if (null? x)
+      y
+  ; If y is not there then just output x.
+  (if (null? y)
+      x
+  (poly_add (cdr x) (cdr y)))))
+
+; Function that adds and puts the results into a list. This code was given in class.
+(define (ladd_helper result x y)
+  (if (null? x)
+      (append result y)
+  (if (null? y)
+      (append result x)
+  (ladd_helper (append result (list (+ (car x) (car y)))) (cdr x) (cdr y)))))
+
+; Function that makes an empty list with lists of x and y. Will call ladd_helper to do the adding. Code was given in class.
+(define (ladd x y)
+  (ladd_helper '() x y))
