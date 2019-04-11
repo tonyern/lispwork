69f493b24041d321e8050004d4c0239787f68e1d
Tony Nguyen <tony.d.nguyen-1@ou.edu>
2019-04-11 11:03:34 -0500

    Saved functions in homework6

--- poly_sub.rkt
+++ poly_sub.rkt
@@ -1,30 +1,35 @@
 #lang racket
 (define (poly_sub x y)
-  ; If x is not there then just output y.
-  (if (null? x)
-      y
-  ; If y is not there then just output x.
-  (if (null? y)
-      x
-  (lsub (car x) (car y))))
-  ; If x is not there then just output y.
-  (if (null? x)
-      y
-  ; If y is not there then just output x.
-  (if (null? y)
-      x
-  (poly_sub (cdr x) (cdr y)))))
+  ; Check for empty list. If empty output something and exit to the next.
+  (if (null? x) y
+  (if (null? y) x
+  ; Appending the results into list here.
+  (append (list (lsub (car x) (car y)))
+  ; Checking for end of list then stopping the program.
+  (if (null? (cdr x)) null
+  ; Move on to the next element in the list until null.
+  (poly_sub (cdr x) (cdr y)))))))
 
-; Helper function for poly_add. x and y are numbers in a list.
-(define (lsub x y)
-  ; Code below was provided in class.
-  ; If x is not there then just output y.
+; Function that adds and puts the results into a list.
+; This code was given in class.
+(define (lsub_helper result x y)
+  ; If x is null and y is not then add y to the list.
   (if (null? x)
-      y
-  ; If y is not there then just output x.
+      (append result y)
+  ; If y is null and x is not then add x to the list.
   (if (null? y)
+      (append result x)
+  ; Computing the values and do a recursive call until the end.
+  (lsub_helper (append result (list (- (car x) (car y)))) (cdr x) (cdr y)))))
+
+; Function that makes an empty list with lists of x and y. Will call ladd_helper to do the adding.
+; This code was given in class.
+(define (lsub x y)
+  (lsub_helper '() x y))
+
+; Removing 0's in the list just contain 0.
+(define (rem0atb x)
+  (if (null? x)
       x
-  ; Start adding numbers in the list.
-  (cons (- (car x) (car y))
-  ; Recursion until end of list.
-  (lsub (cdr x) (cdr y))))))
+  (if (= (car x) 0)
+  (rem0atb (cdr x)) x)))
